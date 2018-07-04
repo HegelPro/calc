e291c182b1e61c5a882e6dd3bc27dda42952ab44
Lev Palkin <palkin.leo@gmail.com>
2018-07-04 18:58:44 +0400

    fix readme.mb

--- Code/webpack.config.js
+++ Code/webpack.config.js
@@ -44,31 +44,31 @@ var clientConfig = (function webpackConfig() {
 module.exports = clientConfig;
 
 
-// module.exports = {
-//   entry: ['./src/app.js', './src/scss/main.scss'],
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ExtractTextPlugin.extract({
-//           fallback: "style-loader",
-//           use: "css-loader"
-//         })
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: ExtractTextPlugin.extract({
-//           fallback: 'style-loader',
-//           use: ['css-loader', 'sass-loader']
-//         })
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new ExtractTextPlugin("styles.css")
-//   ]
-// };
+/* module.exports = {
+  entry: ['./src/app.js', './src/scss/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css")
+  ]
+}; */
